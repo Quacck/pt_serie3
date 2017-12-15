ab023b2f99764701fc03b9bcebc19e4f40ea894a
Vincent Opitz <34582250+Quacck@users.noreply.github.com>
2017-12-15 20:05:47 +0100

    WEIL JULIAN SAUßECODE HAM WILL

---
+++ primefactors.c
@@ -0,0 +1,26 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+int is_prime(int a){
+	
+	for (int i=2; i<=a/2; i++){
+		if (a%i==0){
+		return 0;
+		}		
+
+	}
+	return 1;
+}
+
+int main(int argc, char * argv[]){
+	int n = atoi(argv[1]);
+	for (int i=2; i<=n/2; i++) {
+		if (n%i==0 && is_prime(i)){
+		printf("%d\n",i);	
+		}
+	}
+return 0;
+}
+
+
+
